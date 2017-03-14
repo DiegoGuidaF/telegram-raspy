4c66e2cb4265cc9ff6d2e33b0aef142dc6384f3c
Diego Guida Fórneas <guida.forneas.diego@gmail.com>
2017-03-14 14:22:19 +0100

    Added simple logger

--- main.py
+++ main.py
@@ -8,11 +8,26 @@ from telebot import *
 from sonarr import *
 import time
 import config
+import logging
 
+#Logging system Setup
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# create a file handler
+handler = logging.handlers.RotatingFileHandler('telegram-raspy.log',mode='w',
+                                       maxBytes=20000,backupCount=3)
+handler.setLevel(logging.INFO)
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.info('---------------Telegram-Raspy------------' )
+logger.info('Adding personal info(Token,API and Chat_id)')
 TOKEN = config.TOKEN
 SONARR_API = config.SONARR_API
 CHAT_ID = config.CHAT_ID
 
+logger.info('Initializing telegram bot and sonarr parser')
 telegram = bot(TOKEN)
 sonarr = sonarr(SONARR_API)
 
@@ -36,6 +51,7 @@ def tel_sonarr_grabbed():
 def handle_updates(updates):
     for update in updates["result"]:
         text = update["message"]["text"]
+        logger.info('Message says: "%s"', text)
         if text == "/sonarr":
             keyboard = telegram.build_keyboard(['Sonarr grabbed','Sonarr calendar'])
             telegram.send_message("Select an option", CHAT_ID, keyboard)
@@ -44,19 +60,24 @@ def handle_updates(updates):
             keyboard = telegram.build_keyboard(['/sonarr'])
             telegram.send_message("Server Manager", CHAT_ID, keyboard)
         elif text == "Sonarr calendar":
+            logger.info('SCRIPT: Sending Sonarr calendar')
             tel_sonarr_calendar()
         elif text == "Sonarr grabbed":
+            logger.info('SCRIPT: Sending Sonarr grabbed history')
             tel_sonarr_grabbed()
         else:
+            logger.info('Message not understood')
             telegram.send_message("Sorry, I didn't understand you",CHAT_ID)
 
 
 
 def main():
+    logger.info('Initialized')
     last_update_id = None
     while True:
         updates = telegram.get_updates(last_update_id)
         if len(updates["result"]) > 0:
+            logger.info('Message received, Update_id: %s', str(last_update_id))
             last_update_id = telegram.get_last_update_id(updates) + 1
             handle_updates(updates)
         time.sleep(0.5)
